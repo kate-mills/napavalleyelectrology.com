3b767e14daf1c31fc0298978c3ae6f1c1694647b
Kate Mills <helloworldloveksm@icloud.com>
2024-02-19 10:59:41 -0800

    aftercare instructions

--- src/views/Faq/components/Footer/Footer.js
+++ src/views/Faq/components/Footer/Footer.js
@@ -6,7 +6,7 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Link from 'components/Link'
-import icon from 'images/Electrolysis-Aftercare.pdf'
+import icon from 'images/electrolysis-instructions.pdf'
 
 const Footer = () => {
   const theme = useTheme();
