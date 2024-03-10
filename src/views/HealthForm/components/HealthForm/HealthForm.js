f7c333a1d236a93ee76ba917040cea9de52733eb
Kate Mills <helloworldloveksm@icloud.com>
2024-03-10 16:36:14 -0700

    general information

--- src/views/HealthForm/components/HealthForm/HealthForm.js
+++ src/views/HealthForm/components/HealthForm/HealthForm.js
@@ -7,39 +7,70 @@ import Typography from '@mui/material/Typography'
 import Divider from '@mui/material/Divider'
 import Grid from '@mui/material/Grid'
 import TextField from '@mui/material/TextField'
+import MenuItem from '@mui/material/MenuItem'
 import Button from '@mui/material/Button'
-import Link from 'components/Link'
-import {useTheme} from '@mui/material/styles'
+//import Link from 'components/Link'
+//import {useTheme} from '@mui/material/styles'
 
 import {handleSubmitForm} from 'utils/form-helper'
 
+const yesNoOptions = ['Yes', 'No']
+const phoneRegExp = /[0-9]{3}-?[0-9]{3}-?[0-9]{4}/
+
 const validationSchema = yup.object({
-  firstName: yup
+  fullName: yup
     .string()
     .trim()
-    .min(2, 'Please enter a valid name')
-    .max(50, 'Please enter a valid name')
-    .required('First name is required.'),
-  lastName: yup
-    .string()
-    .trim()
-    .min(2, 'Please enter a valid name')
-    .max(50, 'Please enter a valid name')
-    .required('Last name is required'),
+    .min(2, 'Please enter a valid name.')
+    .max(50, 'Please enter a valid name.')
+    .required('Name is required.'),
+  phoneNumber: yup.string().matches(phoneRegExp, 'Phone number is not valid.').required('Phone number is required.'),
   email: yup.string().trim().email('Please enter a valid email address').required('Email is required.'),
-  skinConcerns: yup.string().trim().max(400, 'Please limit your message to 400 characters')
+  address: yup
+    .string()
+    .trim()
+    .min(10, 'Please enter a valid street address')
+    .max(100, 'Please enter a valid street address')
+    .required('Address is required.'),
+  city: yup
+    .string()
+    .trim()
+    .min(2, 'Please enter a valid city.')
+    .max(50, 'Please enter a valid city.')
+    .required('City is required'),
+  state: yup
+    .string()
+    .trim()
+    .min(2, 'Please enter a valid state abbreviation or name.')
+    .max(20, 'Please enter a valid state name.')
+    .required('State is required.'),
+  zip: yup
+    .string()
+    .trim()
+    .min(5, 'Please enter a valid zip code.')
+    .max(5, 'Please enter a valid zip code.')
+    .required('Zip code is required.'),
+  referredBy: yup.string().trim().min(2, 'Please enter a valid name.').max(50, 'Please enter a valid name.'),
+  skinConcerns: yup.string().trim().max(400, 'Please limit your message to 400 characters.'),
+  hadPastElectro: yup.string().oneOf([...yesNoOptions])
 })
 
 const HealthForm = () => {
-  const theme = useTheme()
+  //const theme = useTheme()
 
   const [formState, setFormState] = useState('')
 
   const initialValues = {
-    firstName: '',
-    lastName: '',
+    fullName: '',
+    phoneNumber: '',
     email: '',
-    skinConcerns: ''
+    address: '',
+    city: '',
+    state: '',
+    zip: '',
+    referredBy: '',
+    skinConcerns: '',
+    hadPastElectro: 'No'
   }
 
   const formik = useFormik({
@@ -55,114 +86,255 @@ const HealthForm = () => {
         console.log(err)
       } finally {
         setFormState(
-          `Thank you, ${values.firstName.trim()}, for taking the time to provide me with your health information. I value your input and look forward to treating you in my office soon.`
+          `Thank you for taking the time to provide me with your health information. I value your input and look forward to treating you in my office soon.`
         )
       }
     }
   })
-  const isDisabled = !formik.values.firstName || !formik.values.lastName || !formik.values.email
+  const isDisabled =
+    !formik.values.fullName ||
+    !formik.values.phoneNumber ||
+    !formik.values.email ||
+    !formik.values.address ||
+    !formik.values.city ||
+    !formik.values.state ||
+    !formik.values.zip ||
+    formState
 
   return (
     <Box maxWidth={600} margin={'0 auto'}>
       <Box marginBottom={4}>
         <Typography variant={'h3'} sx={{fontWeight: 700}} align={'center'} gutterBottom>
-          {!formState ? 'Health Questionnaire' : 'Success'}
+          Health Questionnaire
         </Typography>
         <Typography color="text.secondary" align={formState ? 'left' : 'center'}>
-          {formState || 'Please answer the following questions to the best of your knowledge & ability.'}
+          Please answer the following questions to the best of your knowledge & ability.
         </Typography>
-        {formState && (
-          <Box mt={3}>
-            <Typography variant="body1" gutterBottom textAlign="center" color="text.secondary">
-              {"If you'd like to schedule, "}
-              <Box component={'a'} href={`sms://+14159881102`} color="text.secondary">
-                send me a SMS message
-              </Box>
-              {'.'}
-            </Typography>
-          </Box>
-        )}
       </Box>
-      {!formState && (
-        <Box>
-          <form
-            onSubmit={(event) => {
-              event.preventDefault()
-              formik.handleSubmit()
-            }}
-            id="health-form"
-            name="health-form"
-            data-netlify={'true'}
-            method="POST"
-          >
-            <input type="hidden" name="form-name" value="health-form" />
-            <Grid container spacing={4}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  sx={{height: 54}}
-                  label="First name *"
-                  variant="outlined"
-                  color="primary"
-                  size="medium"
-                  name="firstName"
-                  fullWidth
-                  value={formik.values.firstName}
-                  onBlur={formik.handleBlur}
-                  onChange={formik.handleChange}
-                  error={formik.touched.firstName && Boolean(formik.errors.firstName)}
-                  helperText={formik.touched.firstName && formik.errors.firstName}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  sx={{height: 54}}
-                  label="Last name *"
-                  variant="outlined"
-                  color="primary"
-                  size="medium"
-                  name="lastName"
-                  fullWidth
-                  value={formik.values.lastName}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  error={formik.touched.lastName && Boolean(formik.errors.lastName)}
-                  helperText={formik.touched.lastName && formik.errors.lastName}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  sx={{height: 54}}
-                  label="Email *"
-                  type="email"
-                  variant="outlined"
-                  color="primary"
-                  size="medium"
-                  name="email"
-                  fullWidth
-                  value={formik.values.email}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  error={formik.touched.email && Boolean(formik.errors.email)}
-                  helperText={formik.touched.email && formik.errors.email}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  label="Do you have any spefiic skin concerns?"
-                  multiline
-                  rows={6}
-                  variant="outlined"
-                  color="primary"
-                  size="medium"
-                  name="skinConcerns"
-                  fullWidth
-                  value={formik.values.skinConcerns}
-                  onChange={formik.handleChange}
-                  error={formik.touched.skinConcerns && Boolean(formik.errors.skinConcerns)}
-                  helperText={formik.touched.skinConcerns && formik.errors.skinConcerns}
-                />
-              </Grid>
-              <Grid item container justifyContent={'center'} xs={12}>
+      <Box>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault()
+            formik.handleSubmit()
+          }}
+          id="health-form"
+          name="health-form"
+          data-netlify={'true'}
+          method="POST"
+        >
+          <input type="hidden" name="form-name" value="health-form" />
+
+          <Grid item xs={12} sm={6}>
+            <Typography variant={'subtitle1'} sx={{marginBottom: 2}} fontWeight={700}>
+              General Information
+            </Typography>
+          </Grid>
+          <Grid container spacing={4}>
+            <Grid item xs={12}>
+              <TextField
+                label="Full name"
+                InputLabelProps={{shrink: true}}
+                variant="outlined"
+                color="primary"
+                size="medium"
+                name="fullName"
+                fullWidth
+                value={formik.values.fullName}
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                error={formik.touched.fullName && Boolean(formik.errors.fullName)}
+                helperText={formik.touched.fullName && formik.errors.fullName}
+                sx={{
+                  height: 54,
+                  backgroundColor: 'white !important',
+                  '& input:-internal-autofill-selected': {backgroundColor: 'white !important'}
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                sx={{height: 54}}
+                label="Phone Number"
+                InputLabelProps={{shrink: true}}
+                fullWidth
+                variant="outlined"
+                type="tel"
+                color="primary"
+                size="medium"
+                name="phoneNumber"
+                value={formik.values.phoneNumber}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.phoneNumber && Boolean(formik.errors.phoneNumber)}
+                helperText={formik.touched.phoneNumber && formik.errors.phoneNumber}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                id="email"
+                name="email"
+                InputLabelProps={{shrink: true}}
+                label="Email"
+                type="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
+                sx={{height: 54}}
+                variant="outlined"
+                color="primary"
+                size="medium"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                sx={{height: 54}}
+                label="Address"
+                InputLabelProps={{shrink: true}}
+                variant="outlined"
+                color="primary"
+                size="medium"
+                name="address"
+                id="address"
+                fullWidth
+                value={formik.values.address}
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                error={formik.touched.address && Boolean(formik.errors.address)}
+                helperText={formik.touched.address && formik.errors.address}
+              />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <TextField
+                sx={{height: 54}}
+                label="City"
+                InputLabelProps={{shrink: true}}
+                variant="outlined"
+                color="primary"
+                size="medium"
+                name="city"
+                id="city"
+                fullWidth
+                value={formik.values.city}
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                error={formik.touched.city && Boolean(formik.errors.city)}
+                helperText={formik.touched.city && formik.errors.city}
+              />
+            </Grid>
+
+            <Grid item xs={12} sm={4}>
+              <TextField
+                sx={{height: 54}}
+                InputLabelProps={{shrink: true}}
+                label="State"
+                variant="outlined"
+                color="primary"
+                size="medium"
+                name="state"
+                id="state"
+                fullWidth
+                value={formik.values.state}
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                error={formik.touched.state && Boolean(formik.errors.state)}
+                helperText={formik.touched.state && formik.errors.state}
+              />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <TextField
+                sx={{height: 54}}
+                InputLabelProps={{shrink: true}}
+                label="Zip"
+                variant="outlined"
+                color="primary"
+                size="medium"
+                name="zip"
+                id="zip"
+                fullWidth
+                value={formik.values.zip}
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                error={formik.touched.zip && Boolean(formik.errors.zip)}
+                helperText={formik.touched.zip && formik.errors.zip}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                sx={{height: 54}}
+                InputLabelProps={{shrink: true}}
+                label="Referred By"
+                variant="outlined"
+                color="primary"
+                size="medium"
+                name="referredBy"
+                id="referredBy"
+                fullWidth
+                value={formik.values.referredBy}
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                error={formik.touched.referredBy && Boolean(formik.errors.referredBy)}
+                helperText={formik.touched.referredBy && formik.errors.referredBy}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <Divider />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant={'subtitle1'} marginBottom={-1} fontWeight={700}>
+                General Hygiene
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                InputLabelProps={{shrink: true}}
+                label="Do you have any spefiic skin concerns?"
+                multiline
+                rows={6}
+                variant="outlined"
+                color="primary"
+                size="medium"
+                id="skinConcerns"
+                name="skinConcerns"
+                fullWidth
+                value={formik.values.skinConcerns}
+                onChange={formik.handleChange}
+                error={formik.touched.skinConcerns && Boolean(formik.errors.skinConcerns)}
+                helperText={formik.touched.skinConcerns && formik.errors.skinConcerns}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                select
+                id="hadPastElectro"
+                sx={{height: 54}}
+                label="Have you undergone electrolysis treatments?"
+                variant="outlined"
+                color="primary"
+                size="medium"
+                name="hadPastElectro"
+                fullWidth
+                value={formik.values.hadPastElectro}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.hadPastElectro && Boolean(formik.errors.hadPastElectro)}
+                helperText={formik.touched.hadPastElectro && formik.errors.hadPastElectro}
+              >
+                {yesNoOptions.map((option) => (
+                  <MenuItem key={option} value={option}>
+                    {option}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </Grid>
+
+            <Grid item container justifyContent={'center'} xs={12}>
+              {!formState ? (
                 <Button
                   sx={{height: 54, minWidth: 150}}
                   variant="contained"
@@ -173,58 +345,32 @@ const HealthForm = () => {
                 >
                   Submit
                 </Button>
-              </Grid>
-              <Grid item container justifyContent={'center'} xs={12}>
-                <Typography color="text.secondary"></Typography>
-              </Grid>
-              <Grid item xs={12}>
-                <Divider />
-              </Grid>
-              <Grid item container justifyContent={'center'} xs={12}>
-                <Box>
-                  <Typography component="p" variant="body2" align="left">
-                    By clicking "submit," you agree to our{' '}
-                    <Box
-                      component={Link}
-                      to="/company-terms"
-                      color={theme.palette.text.primary}
-                      fontWeight={'700'}
-                      sx={{textDecoration: 'underline'}}
-                    >
-                      Privacy Policy
-                    </Box>
-                    {', '}
-                    <Box
-                      component={Link}
-                      to="/company-terms"
-                      color={theme.palette.text.primary}
-                      fontWeight={'700'}
-                      sx={{textDecoration: 'underline'}}
-                    >
-                      Data Policy
-                    </Box>
-                    <Box component="span" sx={{display: 'inline-block', marginRight: '2px'}}>
-                      {', and'}
-                    </Box>{' '}
-                    <Box
-                      component={Link}
-                      to="/company-terms"
-                      color={theme.palette.text.primary}
-                      fontWeight={'700'}
-                      sx={{textDecoration: 'underline'}}
-                    >
-                      Cookie Policy
-                    </Box>
-                    .
-                  </Typography>
-                </Box>
-              </Grid>
+              ):<SuccessBlock  msg={formState} />
+              }
             </Grid>
-          </form>
-        </Box>
-      )}
+          </Grid>
+        </form>
+      </Box>
     </Box>
   )
 }
 
 export default HealthForm
+
+const SuccessBlock = ({msg=""}) => {
+  return (
+    <Grid xs={12}>
+      <Typography variant={'h5'} sx={{fontWeight: 700}} align={'center'} mt={2} gutterBottom>
+        Thank you!
+      </Typography>
+      <Typography component="p" variant="body2" align="center">
+        {msg}
+      </Typography>
+      <Typography variant="body2" gutterBottom textAlign="center" color="text.secondary" component="p" mt={3}>
+        <Button variant="contained" component="a" href={`sms://+14159881102`} color="primary">
+          Send me a SMS message
+        </Button>
+      </Typography>
+    </Grid>
+  )
+}
